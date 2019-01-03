8d13e6d921e2f1e47a15490b31de1ccf2498ff6b
florian dahlitz <f2dahlitz@freenet.de>
2019-01-03 21:56:19 +0100

    feat: added two more tests demonstrating the handling of expected exceptions

--- test/tasks/test_api.py
+++ test/tasks/test_api.py
@@ -18,3 +18,18 @@ def test_start_tasks_db():
         exc_msg = exc_info.value.args[0]
 
         assert exc_msg == "db_type must be a 'tiny' or 'mongo'"
+
+
+@pytest.mark.smoke
+def test_list_tasks_wrong_parameter_type():
+    """Test list with wrong parameters."""
+    with pytest.raises(TypeError):
+        tasks.list_tasks(owner=123)
+
+
+@pytest.mark.smoke
+@pytest.mark.get
+def test_get_task_wrong_parameter_type():
+    """Test get using wrong parameters."""
+    with pytest.raises(TypeError):
+        tasks.get(task_id="123")
